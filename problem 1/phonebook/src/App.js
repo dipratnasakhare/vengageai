b31223ce517593192ac52e2c6a54da3cbd47191a
dipratnasakhare <dipratnasakhare11@email.com>
2023-11-30 22:25:37 +0530

    adding phonebook and meet app

---
+++ problem 1/phonebook/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import { InputBox } from "./component/InputBox/InputBox";
+import { ContactBox } from "./component/contactBox/contactBox";
+
+function App() {
+
+
+
+
+
+  return (
+    <div className="App">
+
+      <ContactBox />
+
+      <InputBox />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ problem 1/phonebook/src/component/InputBox/InputBox.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const InputBox = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
